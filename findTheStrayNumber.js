99b9f39b56d00301a4667da113c27489796df418
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-04-19 23:46:52 -0400

    Created findTheStrayNumber.js

---
+++ findTheStrayNumber.js
@@ -0,0 +1,29 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+
+
+function stray(numbers) {
+    const map = {}
+
+    for (num of numbers) {
+        if (!map[num]) {
+            map[num] = 0
+        }
+        map[num]++
+    }
+
+    for (key in map) {
+
+        if (map[key] === 1) {
+            return Number(key)
+        }
+    }
+}
