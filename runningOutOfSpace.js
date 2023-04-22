37b0ac393afb77fc5a6a15bb8a1740dec756d2e3
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-04-21 21:48:54 -0400

    Created runningOutOfSpace.js

---
+++ runningOutOfSpace.js
@@ -0,0 +1,10 @@
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+// For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+function spacey(array) {
+    let newWord = ""
+
+    const noSpace = array.map(el => newWord += el)
+
+    return noSpace
+}
