676131a4fc3b32ad0e498d2145dca89e88386ce3
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-07-08 22:39:10 -0400

    Created findTheVowels.js

---
+++ findTheVowels.js
@@ -0,0 +1,28 @@
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+
+
+
+function vowelIndices(word) {
+    const result = []
+    const lowerCase = word.toLowerCase()
+
+
+
+    for (let i = 0; i < lowerCase.length; i++) {
+        if (lowerCase[i] === "a" || lowerCase[i] === "e" || lowerCase[i] === "i" || lowerCase[i] === "o" || lowerCase[i] === "u" || lowerCase[i] === "y") {
+            result.push(i + 1)
+        }
+    }
+
+    return result
+
+
+}
